0daf8a2d9ac4dcc0f6e2121b8260eadaa421b1dc
Vuong Do <vuongdope@gmail.com>
2019-06-01 03:41:22 -0700

    Added react hook to menu component

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,16 +6,7 @@ class Container extends Component {
     constructor(props){
         super(props);
         this.state = {
-            styles: {
-                position: '',
-                top: '',
-                backgroundColor: '',
-                padding: '',
-                display: '',
-                right: '',
-                left: '',
-                zIndex: ''
-            }
+            styles: ''
         };
     }
 
@@ -32,49 +23,43 @@ class Container extends Component {
     }
 
     morphType = () => {
-        const {styles} = this.state;
         const {type} = this.props;
-        const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-        const scrollPos = window.pageYOffset;
-        
-        let typeStyle = {};
+        // const {styles} = this.state;
+        // const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        // const scrollPos = window.pageYOffset;
+
+        let style = "";
 
         switch(type){
             case 'fixedNavbar':
+                // let visible = false;
+                // typeStyle.backgroundColor = '#59c2ff';
+                // typeStyle.position = 'fixed';
 
-                let visible = false;
-                typeStyle.backgroundColor = '#59c2ff';
-                typeStyle.position = 'fixed';
-
-                if(scrollPos > 45 + height*(0.005) && visible === false){
-                    visible = true;
-                }else if(scrollPos <= 45 + height*(0.005) && visible === true){
-                    visible = false;
-                }
+                // if(scrollPos > 45 + height*(0.005) && visible === false){
+                //     visible = true;
+                // }else if(scrollPos <= 45 + height*(0.005) && visible === true){
+                //     visible = false;
+                // }
                 
-                if(visible){
-                    typeStyle.top = 0;
-                }else if(!visible){
-                    typeStyle.top = -(45 + height*(0.006));
-                }
+                // if(visible){
+                //     typeStyle.top = 0;
+                // }else if(!visible){
+                //     typeStyle.top = -(45 + height*(0.006));
+                // }
 
             break;
             case 'staticNavbar':
-                typeStyle.position = "sticky";
-                typeStyle.top = 0;
-                typeStyle.zIndex = 1;
-                typeStyle.backgroundColor = "#59c2ff";
+                style = "static-navbar";
             break
             case 'innerContent':
-                typeStyle.padding = 15;
+                style = "inner-content";
             break;
             default:
             break;
         }
 
-        const newStyles = Object.assign({...styles}, typeStyle);
-
-        this.setState({styles: newStyles});
+        this.setState({styles: style});
 
     }
 
@@ -84,7 +69,7 @@ class Container extends Component {
 
         return(
 
-            <section className="container" style={{display: styles.display, position: styles.position, top: styles.top, backgroundColor: styles.backgroundColor, padding: styles.padding, zIndex: styles.zIndex}}>
+            <section className={`container ${styles}`}>
                 {this.props.children}
             </section>
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,62 +1,106 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect, useContext} from 'react';
 import { Link } from 'react-router-dom';
 import './Menu.scss';
 import {NavbarContext} from './NavbarContext';
 
-class Menu extends Component {
+// hook component
+function Menu(props){
 
-    constructor(props){
-        super(props);
-        this.state = {};
-        this.mql = window.matchMedia('(max-width: 700px)');
-    }
+    const [mql, setMql] = useState(window.matchMedia('(max-width: 700px)'));
+    const navbar = useContext(NavbarContext);
 
-    componentDidMount(){
+    useEffect(() => {
 
-        this.mql.addListener(this.MediaHandler);
+        mql.addListener(MediaHandler);
 
-    }
-    
-    componentWillUnmount(){
+        return (() => mql.removeListener(MediaHandler));
 
-        this.mql.removeListener(this.MediaHandler);
+    });
 
-    }
-
-    MediaHandler = (event) => {
-        console.log(this.props);
-        if(!event.matches && this.context.showMenu){
-            console.log("here");
-            this.props.toggleMenu({
+    function MediaHandler(event){
+        if(!event.matches && navbar.showMenu){
+            props.toggleMenu({
                 target: {className: "menu"},
                 currentTarget: {className: "menu"}
             });
         }
     }
 
-    render(){
-        return(
-            <div className="menu-container">
-                <div className="portfolio" >
-                    Portfolio {this.context.showPort.toString()}
-                </div>
-                <div className="services">
-                    Services
-                </div>
-                <div className="about">
-                    About
-                </div>
-                <Link to='/contact'>
-                    <div className="contact">
-                            Contact
-                    </div>
-                </Link>    
+    return(
+        <div className="menu-container">
+            <div className="portfolio" >
+                Portfolio {navbar.showPort.toString()}
             </div>
-        );
-    }
+            <div className="services">
+                Services
+            </div>
+            <div className="about">
+                About
+            </div>
+            <Link to='/contact'>
+                <div className="contact">
+                        Contact
+                </div>
+            </Link>    
+        </div>
+    );
 
 }
 
-Menu.contextType = NavbarContext;
+// class Menu extends Component {
+
+//     constructor(props){
+//         super(props);
+//         this.state = {};
+//         this.mql = window.matchMedia('(max-width: 700px)');
+//     }
+
+//     componentDidMount(){
+
+//         this.mql.addListener(this.MediaHandler);
+
+//     }
+    
+//     componentWillUnmount(){
+
+//         this.mql.removeListener(this.MediaHandler);
+
+//     }
+
+//     MediaHandler = (event) => {
+//         console.log(this.props);
+//         if(!event.matches && this.context.showMenu){
+//             console.log("here");
+//             this.props.toggleMenu({
+//                 target: {className: "menu"},
+//                 currentTarget: {className: "menu"}
+//             });
+//         }
+//     }
+
+//     render(){
+//         return(
+//             <div className="menu-container">
+//                 <div className="portfolio" >
+//                     Portfolio {this.context.showPort.toString()}
+//                 </div>
+//                 <div className="services">
+//                     Services
+//                 </div>
+//                 <div className="about">
+//                     About
+//                 </div>
+//                 <Link to='/contact'>
+//                     <div className="contact">
+//                             Contact
+//                     </div>
+//                 </Link>    
+//             </div>
+//         );
+//     }
+
+// }
+
+// Menu.contextType = NavbarContext;
 
 export default Menu;
